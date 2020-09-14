7990579a2fd0161880ffb49639055f97a05e3a02
komal <lifebeat04@gmail.com>
2020-09-14 22:48:51 +0530

    commit

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
-    create = models.DateTimeField(auto_now_add=True)
+    #create = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
